093c6ec821f00a242fad6d312e6d12400f67171b
Danilo <developer@danilo.info>
2016-07-11 06:34:47 -0300

    Minor changes on interfaces

--- application/backend/model/interfaces.go
+++ application/backend/model/interfaces.go
@@ -2,73 +2,73 @@ package model
 
 type(
 	UserAccount interface {
-		Create(userAccountData *UserAccountData) error
+		Create(data *UserAccountData) (err error)
 		GetByUsername(username string) (*UserAccountData, error)
 		GetByID(id int64) (*UserAccountData, error)
-		DeleteByID(id int64) error
-		Update(userAccountData *UserAccountData) error
+		DeleteByID(id int64) (err error)
+		Update(data *UserAccountData) (err error)
 	}
 
 	UserPassword interface {
-		Create(userPasswordData *UserPasswordData) error
-		Update(userPasswordData *UserPasswordData) error
-		DeleteByID(id int64) error
+		Create(data *UserPasswordData) (err error)
+		Update(data *UserPasswordData) (err error)
+		DeleteByID(id int64) (err error)
 		GetByID(id int64) (*UserPasswordData, error)
 	}
 
 	UserEmail interface {
-		Create(userEmailData *UserEmailData) error
-		Update(userEmailData  *UserEmailData) error
-		DeleteByAddress(address string) error
+		Create(data *UserEmailData) (err error)
+		Update(data *UserEmailData) (err error)
+		DeleteByAddress(address string) (err error)
 		GetByAddress(address string) (*UserEmailData, error)
 	}
 
 	UserPersonalInformation interface {
-		Create(userPersonalInformation *UserPersonalInformation) error
-		Update(userPersonalInformation  *UserPersonalInformation) error
-		DeleteById(id int64) error
+		Create(userPersonalInformation *UserPersonalInformation) (err error)
+		Update(userPersonalInformation  *UserPersonalInformation) (err error)
+		DeleteById(id int64) (err error)
 		GetById(id int64) (*UserPersonalInformation, error)
 	}
 	Module interface {
-		Create(moduleData *ModuleData) error //missing model data
+		Create(data *ModuleData) (err error)
 		GetByID(id int64) (*ModuleData, error)
-		DeleteByID(id int64) error
-		Update(moduleData *ModuleData) error
+		DeleteByID(id int64) (err error)
+		Update(data *ModuleData) (err error)
 	}
 	Operation interface {
-		Create(operationData *OperationData) error
+		Create(data *OperationData) (err error)
 		GetByID(id int64) (*OperationData, error)
-		DeleteByID(id int64) error
-		Update(OperationData *OperationData) error
+		DeleteByID(id int64) (err error)
+		Update(data *OperationData) (err error)
 	}
 
 	Permission interface {
-		Create(permissionData *PermissionData) error
+		Create(data *PermissionData) (err error)
 		GetByID(id int64) (*PermissionData, error)
-		DeleteByID(id int64) error
-		Update(permissionData *PermissionData) error
+		DeleteByID(id int64) (err error)
+		Update(data *PermissionData) (err error)
 	}
 
 	Role interface {
-		Create(roleData *RoleData) error
+		Create(data *RoleData) (err error)
 		GetByID(id int64) (*RoleData, error)
-		DeleteByID(id int64) error
-		Update(roleData *RoleData) error
-		HasPermission(roleData *RoleData, operationPath string) (bool, error) // TODO:Maybe we should not tell when a op exists
+		DeleteByID(id int64) (err error)
+		Update(data *RoleData) (err error)
+		HasPermission(data *RoleData, operationPath string) (bool, error)
 	}
 
 	OperationPermissionManager interface {
-		SetPermissionOperations(permissionID int64, operationIDs []int64) error
-		AddPermissionOperations(permissionID int64, operationIDs  []int64) error
-		RemovePermissionOperations(permissionID int64, operationIDs  []int64) error
+		SetPermissionOperations(permissionID int64, operationIDs []int64) (err error)
+		AddPermissionOperations(permissionID int64, operationIDs  []int64) (err error)
+		RemovePermissionOperations(permissionID int64, operationIDs  []int64) (err error)
 		GetOperationPermissions(operationID int64) ([]*PermissionData, error)
 		GetPermissionOperations(permissionID int64) ([]*OperationData, error)
 	}
 
 	PermissionRoleManager interface {
-		SetRolePermissions(roleID int64, permissionsIDs []int64) error
-		AddRolePermissions(roleID int64, permissionsIDs []int64) error
-		RemoveRolePermissions(roleID int64, permissionsIDs []int64) error
+		SetRolePermissions(roleID int64, permissionsIDs []int64) (err error)
+		AddRolePermissions(roleID int64, permissionsIDs []int64) (err error)
+		RemoveRolePermissions(roleID int64, permissionsIDs []int64) (err error)
 		GetPermissionRoles(permissionID int64) ([]*RoleData, error)
 		GetRolePermissions(roleID int64) ([]*PermissionData, error)
 	}
