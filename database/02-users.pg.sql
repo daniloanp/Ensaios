798035397af8266600b0cf206e9807c8d72399fb
Danilo <developer@danilo.info>
2016-07-22 14:35:42 -0300

    More Refactoring

--- database/02-users.pg.sql
+++ database/02-users.pg.sql
@@ -4,69 +4,68 @@
 SET search_path TO users;
 
 CREATE TABLE nickname (
-    id BIGSERIAL NOT NULL,
-    name VARCHAR(255) NOT NULL,
+    id                    BIGSERIAL                   NOT NULL,
+    name                  VARCHAR(255)                NOT NULL,
     registration_datetime TIMESTAMP(2) WITH TIME ZONE NOT NULL DEFAULT current_timestamp(2),
     -- table constraints
-    PRIMARY KEY(id),
-    CONSTRAINT chk_nickname_name_rule CHECK(name::text ~ '^[a-z][a-z0-9\\-_.]*$' AND  length(name) >= 3),
-    CONSTRAINT uq_nickname_name UNIQUE(name)
+    CONSTRAINT pk_nickname PRIMARY KEY (id),
+    CONSTRAINT chk_nickname__name__rule CHECK (name :: TEXT ~ '^[a-z][a-z0-9\\-_.]*$' AND length(name) >= 3),
+    CONSTRAINT uq_nickname__name UNIQUE (name)
 );
 
 CREATE TABLE user_account (
     -- table columns with their constraints
-    id                    BIGSERIAL NOT NULL,
+    id                    BIGSERIAL                   NOT NULL,
     registration_datetime TIMESTAMP(2) WITH TIME ZONE NOT NULL DEFAULT current_timestamp(2),
     -- table constraints
---    CONSTRAINT uq_username UNIQUE (username),
-    PRIMARY KEY (id)
+    --    CONSTRAINT uq_username UNIQUE (username),
+    CONSTRAINT pk_user_account PRIMARY KEY (id)
 );
 
 CREATE TABLE user_nickname (
-    id BIGSERIAL NOT NULL,
-    user_account_id BIGINT NOT NULL,
-    nickname_id BIGINT NOT NULL,
+    id                    BIGSERIAL                   NOT NULL,
+    user_account_id       BIGINT                      NOT NULL,
+    nickname_id           BIGINT                      NOT NULL,
     registration_datetime TIMESTAMP(2) WITH TIME ZONE NOT NULL DEFAULT current_timestamp(2),
     -- Table constraint
-    PRIMARY KEY(id),
-    UNIQUE(id, user_account_id, nickname_id),
-    CONSTRAINT fk_user_nickname_user_account_id FOREIGN KEY(user_account_id) REFERENCES user_account(id),
-    CONSTRAINT fk_user_nickname_nickname_id FOREIGN KEY(nickname_id) REFERENCES nickname(id)
+    CONSTRAINT pk_user_nickname PRIMARY KEY (id),
+    CONSTRAINT uq_user_nickname__id_user_nickname UNIQUE (id, user_account_id, nickname_id),
+    CONSTRAINT fk_user_nickname__user_account_id FOREIGN KEY (user_account_id) REFERENCES user_account (id),
+    CONSTRAINT fk_user_nickname__nickname_id FOREIGN KEY (nickname_id) REFERENCES nickname (id)
 );
 
 CREATE TABLE user_password (
     -- table columns with their constraints
-    id                    BIGSERIAL NOT NULL,
+    id                    BIGSERIAL                   NOT NULL,
     user_account_id       BIGINT                      NOT NULL,
     password              TEXT                        NOT NULL,
     salt                  TEXT                        NOT NULL,
     registration_datetime TIMESTAMP(2) WITH TIME ZONE NOT NULL DEFAULT current_timestamp(2),
     -- table constraints
-    PRIMARY KEY (id),
-    UNIQUE (id, user_account_id), -- TODO:CHECK_BEST_WAY
-    FOREIGN KEY (user_account_id) REFERENCES user_account (id)
+    CONSTRAINT pk_user_password PRIMARY KEY (id),
+    CONSTRAINT uq_user_password__id_user_account UNIQUE (id, user_account_id),
+    CONSTRAINT fk_user_password__user_account FOREIGN KEY (user_account_id) REFERENCES user_account (id)
 );
 
-CREATE TABLE email(
-    id BIGSERIAL NOT NULL,
-    address VARCHAR(255) NOT NULL,
+CREATE TABLE email (
+    id                    BIGSERIAL                   NOT NULL,
+    address               VARCHAR(255)                NOT NULL,
     registration_datetime TIMESTAMP(2) WITH TIME ZONE NOT NULL DEFAULT current_timestamp(2),
     -- table constraints
-    PRIMARY KEY(id),
---    CONSTRAINT chk_email_address_rule CHECK(address::text ~ '^[a-z][a-z0-9\\-_.]*$' AND  length(address) >= 3),
-    CONSTRAINT uq_email_address UNIQUE(address)
+    CONSTRAINT pk_email PRIMARY KEY (id),
+    CONSTRAINT uq_email__address UNIQUE (address)
 );
 
 CREATE TABLE user_email (
     -- table columns with their constraints
     user_account_id       BIGINT                      NOT NULL,
-    email_id              BIGINT               NOT NULL,
+    email_id              BIGINT                      NOT NULL,
     verified              BOOLEAN                              DEFAULT FALSE,
     registration_datetime TIMESTAMP(2) WITH TIME ZONE NOT NULL DEFAULT current_timestamp(2),
     -- table constraints
-    FOREIGN KEY(email_id) REFERENCES email(id),
-    FOREIGN KEY (user_account_id) REFERENCES user_account (id),
-    PRIMARY KEY (user_account_id, email_id)
+    CONSTRAINT fk_user_email__email FOREIGN KEY (email_id) REFERENCES email (id),
+    CONSTRAINT fk_user_email__user_account FOREIGN KEY (user_account_id) REFERENCES user_account (id),
+    CONSTRAINT pk_user_email PRIMARY KEY (user_account_id, email_id)
 );
 
 CREATE TABLE user_personal_information (
@@ -80,37 +79,34 @@ CREATE TABLE user_personal_information (
     nationality           TEXT                                 DEFAULT NULL,
     registration_datetime TIMESTAMP(2) WITH TIME ZONE NOT NULL DEFAULT current_timestamp(2),
     -- table constraints
-    PRIMARY KEY (id),
-    UNIQUE(id, user_account_id),
-    FOREIGN KEY (user_account_id) REFERENCES user_account (id)
+    CONSTRAINT pk_user_personal_information PRIMARY KEY (id),
+    CONSTRAINT uq_personal_information__id_user_account_id UNIQUE (id, user_account_id),
+    CONSTRAINT fk_user_personal_information__user_account FOREIGN KEY (user_account_id) REFERENCES user_account (id)
 );
 
-CREATE TABLE user_current_email ( -- several
-    user_account_id    BIGINT NOT NULL,
-    user_email_id      BIGINT NOT NULL,
-    UNIQUE (user_email_id),
-    FOREIGN KEY (user_account_id) REFERENCES user_account (id),
-    FOREIGN KEY (user_email_id, user_account_id) REFERENCES user_email (email_id, user_account_id)
+CREATE TABLE user_current_email (-- several
+    user_account_id BIGINT NOT NULL,
+    user_email_id   BIGINT NOT NULL,
+    CONSTRAINT uq_user_current_email__user_email_id UNIQUE (user_email_id),
+    CONSTRAINT fk_user_current_email__user_account FOREIGN KEY (user_account_id) REFERENCES user_account (id),
+    CONSTRAINT fk_user_current_email__user_email FOREIGN KEY (user_email_id, user_account_id) REFERENCES user_email (email_id, user_account_id)
 );
-
 
 CREATE TABLE user_current_nickname (
     user_nickname_id BIGINT NOT NULL,
-    nickname_id BIGINT NOT NULL,
-    user_account_id BIGINT NOT NULL,
-    PRIMARY KEY(user_nickname_id),
+    nickname_id      BIGINT NOT NULL,
+    user_account_id  BIGINT NOT NULL,
+    CONSTRAINT pk_user_current_nickname PRIMARY KEY (user_nickname_id),
     UNIQUE (user_account_id),
     UNIQUE (nickname_id),
-    FOREIGN KEY (user_nickname_id, nickname_id, user_account_id) REFERENCES user_nickname(id, nickname_id, user_account_id)
-
-
+    CONSTRAINT fk_user_current_nickname__user_nickname FOREIGN KEY (user_nickname_id, nickname_id, user_account_id) REFERENCES user_nickname (id, nickname_id, user_account_id)
 );
 -- table defines wich user_account is the current information;
 CREATE TABLE user_current_information (
     -- table columns with their constraints
     user_account_id              BIGINT NOT NULL,
     user_password_id             BIGINT DEFAULT NULL,
-    user_personal_information_id BIGINT DEFAULT NULL,    -- table constraints
+    user_personal_information_id BIGINT DEFAULT NULL, -- table constraints
     PRIMARY KEY (user_account_id),
     UNIQUE (user_personal_information_id),
     FOREIGN KEY (user_account_id) REFERENCES user_account (id),
--- database/Readme.md
+++ database/Readme.md
@@ -1,15 +1,3 @@
-### Modelo de dados.
-Se tratando de um CMS, informações sobre permissão e distribuição de dados, templates HTML e arquivos precisam estar descritas em um banco de dados pois podem ser alteradas dinamicamente.
+### Database rules.
 
-Este banco é uma proposta de modelo que viabiliza o controle de permissão utilizando role-based access control (RBAC) e um modelo de descrição de dados que agrupa informações em views. Tenta-se também, adquirir um modelo que salve e mantenha um registro de estados anteriores, para ter controle de versão de conteúdo e de dados sensíveis.
-
-Foram dividos em quatro schemas SQL (namespaces): Controller, Session Management, Content e Users.
-
-O Controller controla a permissão para determinadas operações.
-O Session Management interliga o módulo usuário ao Controller
-O Usuário persiste dados dos usuários e de login.
-O Content armazena e agrupa dados em vizualizações.
-
-Existe certo acoplamento entre esses "modulos", uma vez que existem referêcias entre eles.
-
-A ideia é que se implementa uma camada de abstração OO para esse modelo conveniente, não necessáriamente um mapeamento direto (isto é, nem toda tabela vai produzir uma classe);
+- Please
